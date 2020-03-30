b10064a9cebe66bfeb9cb37e25fa254a98190dbc
agrechuha <agrechuha@gmail.com>
2020-03-30 13:50:22 +1000

    edit arrows in sliders (cross with text in mobile view) agrechuha

--- src/js/video.js
+++ src/js/video.js
@@ -1,1 +1,6 @@
-$('.video-js').addClass('active');
+$(document).ready(function() {
+    $('.video-js').get(0).play();
+    $('.video-js').get(1).play();
+    $('.video-js').get(2).play();
+    $('.video-js').get(3).play();
+});
