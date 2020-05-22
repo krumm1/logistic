13aef6a04e133edc097e5a77608ceda720532db1
agrechuha <agrechuha@gmail.com>
2020-05-22 12:29:01 +1000

    edit time function (without date) in header agrechuha

--- src/js/script.js
+++ src/js/script.js
@@ -81,21 +81,14 @@ $(document).ready(() => {
 
 	function clockInit() {
 		let now = new Date();
-		let date = document.getElementById("header-date");
 		let time = document.getElementById("header-time");
-		let day = now.getDate();
-		let month = now.getMonth() + 1;
 		let hours = now.getHours();
 		let minutes = now.getMinutes();
-		if (day < 10)
-			day = "0" + day;
-		if (month < 10)
-			month = "0" + month;
+
 		if (hours < 10)
 			hours = "0" + hours;
 		if (minutes < 10)
 			minutes = "0" + minutes;
-		date.innerHTML = day + '/' + month;
 		time.innerHTML = hours + ':' + minutes;
 		setTimeout(clockInit, 20000);
 	};
